baa5c527554b7b6bce7718b1a93d18cbf24a3133
博 <111757980+jyb040329@users.noreply.github.com>
2024-10-01 09:39:51 +0800

    Update index.js

--- Web/015/js/index.js
+++ Web/015/js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||郭组长|祝你|生日快乐！|❤|#rectangle|');
+      S.UI.simulate('|#countdown 3||赵欣|祝你|国庆节快乐！|❤|#rectangle|');
     }
 
     S.Drawing.loop(function () {
